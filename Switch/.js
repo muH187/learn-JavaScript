88a2a6259aa0e29ac41894418f8bbf5a751323e6
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-31 18:37:49 +0500

    swicth example with day

---
+++ Switch/.js
@@ -0,0 +1,27 @@
+let day = 3
+
+switch(day) {
+    case 1:
+        console.log("It's Monday")
+        break
+    case 2:
+        console.log("It's Tuesday")
+        break
+    case 3:
+        console.log("It's Wednessday")
+        break
+    case 4:
+        console.log("It's Thursday")
+        break
+    case 5:
+        console.log("It's Friday")
+        break
+    case 6:
+        console.log("It's Saturday")
+        break
+    case 7:
+        console.log("It's Sunday")
+        break
+    default:
+        console.log(`${day} is not a day!`)
+}
