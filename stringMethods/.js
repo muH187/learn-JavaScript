33f963e20ce8e89ca5a5daf9054fc5cf58665bcb
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-31 21:04:36 +0500

    string methods

---
+++ stringMethods/.js
@@ -0,0 +1,12 @@
+let userName = "Muhammad Ali"
+
+console.log(userName.charAt(11))
+console.log(userName.indexOf("i"))
+
+userName = userName.trim()
+
+let phoneNumber = "123-123-573-532"
+
+phoneNumber = phoneNumber.replaceAll("-", " - ")
+
+console.log(phoneNumber)
