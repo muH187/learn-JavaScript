a31254f91f0862235f558b803b28879c20eaabc7
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-01 18:39:51 +0500

    temperature convertor

---
+++ TempConversion/script.js
@@ -0,0 +1,22 @@
+document.title = "Temperature Conversion"
+
+const textBox = document.getElementById("textBox")
+const result = document.getElementById("result")
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelsius = document.getElementById("toCelsius")
+
+let temp
+
+function convert() {
+    if(toFahrenheit.checked) {
+        temp = Number(textBox.value)
+        temp = temp * 9 / 5 + 32
+        result.textContent = temp.toFixed(1) + "'F"
+    } else if(toCelsius.checked) {
+        temp = Number(textBox.value)
+        temp = (temp - 32) * (5/9)
+        result.textContent = temp.toFixed(1) + "'C"
+    } else {
+        result.textContent = "Please select unit."
+    }
+}
