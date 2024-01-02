41b8ca4d2cc6ba803312c99fc78e5c78c79a6fc2
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-02 23:53:22 +0500

    more on ternary

--- ternary/.js
+++ ternary/.js
@@ -3,3 +3,9 @@ let age = 19
 let message = age >= 18 ? "You are an adult" : "You are a minor"
 
 console.log(message)
+
+let purchaseAmount = 80
+
+let discount = purchaseAmount < 100 ? "You got 10% discount" : "You didn't get discount"
+
+console.log(discount)
