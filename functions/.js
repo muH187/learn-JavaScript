751d7bc961d3488d9e501a8db2181992063e5d28
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-02 23:16:40 +0500

    functions

---
+++ functions/.js
@@ -0,0 +1,20 @@
+function showDetails(name, city, country, age) {
+    console.log(`My name is ${name}`)
+    console.log(`I'm from ${city} ${country}`)
+    console.log(`I'm ${age} years old`)
+}
+
+showDetails("Muhammad Ali", "Karachi", "Pakistan", "19")
+showDetails("Muhammad Mirza", "Islamabad", "Pakistan", "19")
+
+function add(x, y) {
+    return x + y
+}
+
+console.log(add(2, 5))
+
+function subtract(y, x) {
+    return x - y
+}
+
+console.log(subtract(10, 5))
