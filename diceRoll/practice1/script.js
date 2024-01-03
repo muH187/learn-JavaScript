04c1f1153e56799328dc251bef53651efda2ca5c
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-03 12:22:37 +0500

    pending project

---
+++ diceRoll/practice1/script.js
@@ -0,0 +1,19 @@
+document.title = "Dice Roller"
+
+function rollDice() {
+    const numbOfDice = document.getElementById("numbOfDice").value
+    const diceResult = document.getElementById("diceResult")
+    const diceImages = document.getElementById("diceImages")
+
+    const values = []
+    const images = []
+
+    for (let i = 0; i < numbOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1
+        values.push(value)
+        images.push(`<img src="images/${value}.png" alt="Dice ${value}">`)
+    }
+
+    diceResult.textContent = `dice: ${values.join(", ")}`
+    diceImages.innerHTML = images.join("")
+}
