0d244c56ae1b79208e033e36d226e360cfe75243
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-04 21:46:11 +0500

    random password generator

---
+++ passGenerator/project1/script.js
@@ -0,0 +1,43 @@
+const passwordLenght = 10
+const includeUppercase = true
+const includeLowercase = true
+const includeNumbers = true
+const includeSymbols = true
+
+function generatedPassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numberChars = "0123456789"
+    const symbolChars = "!@#$%^&*()_+-<>?"
+
+    let allowedChars = ""
+    let password = ""
+
+    allowedChars += includeLowercase ? lowercaseChars : ""
+    allowedChars += includeUppercase ? uppercaseChars : ""
+    allowedChars += includeNumbers ? numberChars : ""
+    allowedChars += includeSymbols ? symbolChars : ""
+
+    if(length <= 0) {
+        return (`password length must be atleast 1`)
+    }
+    if(allowedChars.length === 0) {
+        return (`At least 1 set of characters needs to be selected`)
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length)
+        password += allowedChars[randomIndex]
+    }
+    return password
+}
+
+const password = generatedPassword(
+    passwordLenght, 
+    includeUppercase,
+    includeLowercase,
+    includeNumbers,
+    includeSymbols)
+
+console.log(`Generated Password: ${password}`);
