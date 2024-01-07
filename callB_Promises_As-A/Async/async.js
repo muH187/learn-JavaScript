15808ad64802122727c667ec8aab99bd6162fb6c
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-07 22:30:30 +0500

    more on async await

--- callB_Promises_As-A/Async/async.js
+++ callB_Promises_As-A/Async/async.js
@@ -11,3 +11,23 @@ async function getData() {
     await api()
     await api()
 }
+
+// More on Async Await
+
+function fetchData(dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data", dataId)
+            resolve("success")
+        }, 2000)
+    })
+}
+
+async function forData() {
+    console.log("Fetching data 1....");
+    await fetchData(1)
+    console.log("Fetching data 2....");
+    await fetchData(2)
+    console.log("Fetching data 3....");
+    await fetchData(3)
+}
