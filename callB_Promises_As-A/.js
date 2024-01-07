a2e182eff5bb9489e93a43d3a41ea1985044ef5f
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-07 18:44:13 +0500

    time out

---
+++ callB_Promises_As-A/.js
@@ -0,0 +1,11 @@
+function greeting() {
+    console.log("Hello Muhammad Ali! How are you?");
+}
+
+setTimeout(greeting, 2000)
+
+function answer() {
+    console.log("Hi! Thanks for asking, I'm fine what about you?");
+}
+
+setTimeout(answer, 4000)
