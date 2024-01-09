f2fe4db84bf9ce6c3982be7ef8e70627c9a7ba8e
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-09 16:00:18 +0500

    rock paper scissors game complete

---
+++ RockPaperScissors/project1/script.js
@@ -0,0 +1,61 @@
+// ====== DOM
+const choices = document.querySelectorAll(".choice")
+const msg = document.querySelector("#msg")
+const userScorePara = document.querySelector("#user_score")
+const compScorePara = document.querySelector("#computer_score")
+
+// ====== Variables
+let userScore = 0
+let compScore = 0
+
+// ====== Functions
+function getCompChoice() {
+    const options = ["rock", "paper", "scissors"]
+    const randIndx = Math.floor(Math.random() * 3)
+    return options[randIndx]
+}
+
+function drawGame() {
+    msg.textContent = "Game Draw! Play Again"
+    msg.style.backgroundColor = "#365486"
+} 
+
+function showWin(userWin, userChoice, compChoice) {
+    if(userWin) {
+        userScore++
+        userScorePara.textContent = userScore
+        msg.textContent = `You win! Your ${userChoice} beats ${compChoice}`
+        msg.style.backgroundColor = "green"
+    } else {
+        compScore++
+        compScorePara.textContent = compScore
+        msg.textContent = `You lose! ${userChoice} beats your ${compChoice}`
+        msg.style.backgroundColor = "red"
+    }
+}
+
+// ====== Logic
+const playGame = (userChoice) => {
+    const compChoice = getCompChoice()
+
+    if(userChoice === compChoice) {
+        drawGame()
+    } else {
+        let userWin = true
+        if(userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true
+        } else {
+            userWin = compChoice === "rock" ? false : true
+        }
+        showWin(userWin, userChoice, compChoice)
+    }
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id")
+        playGame(userChoice)
+    }) 
+})
