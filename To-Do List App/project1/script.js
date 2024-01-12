854f1187e94665a15ce14de55b74c6377eae7d51
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-12 16:36:35 +0500

    project has been completed

---
+++ To-Do List App/project1/script.js
@@ -0,0 +1,40 @@
+const inbox = document.querySelector("#inbox_box")
+const listContainer = document.querySelector(".list_container")
+
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+btn.addEventListener("click", () => {
+    if(inbox === ""){
+        window.alert("You must write something!")
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inbox.value
+        listContainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+
+    }
+    inbox.value = ""
+    saveData()
+})
+
+showTask()
