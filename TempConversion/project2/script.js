9a2efc35448da0bf5772be8841a7e731dd14644d
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-21 16:25:51 +0500

    doing correction

--- TempConversion/project2/script.js
+++ TempConversion/project2/script.js
@@ -1,21 +1,21 @@
-const sumbit = document.querySelector("#submit")
-const textBox = document.querySelector("#textBox")
-const result = document.querySelector("#result")
-const toFahrenheit = document.querySelector("#toFahrenheit")
-const toCelsius = document.querySelector("#toCelsius")
+const submit = document.querySelector("#submit");
+const textBox = document.querySelector("#textBox");
+const result = document.querySelector("#result");
+const toFahrenheit = document.querySelector("#toFahrenheit");
+const toCelsius = document.querySelector("#toCelsius");
 
-let temp
+let temp;
 
 submit.addEventListener("click", () => {
-    if(toFahrenheit.checked) {
-        temp = Number(textBox.value)
-        temp = temp * 9 / 5 + 32
-        result.textContent = temp.toFixed(1) + "`F"
+    if (toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = (temp * 9) / 5 + 32;
+        result.textContent = temp.toFixed(1) + "'F";
     } else if (toCelsius.checked) {
-        temp = Number(textBox.value)
-        temp = (temp - 32) * (5/9)
-        result.textContent = temp.toFixed(1) + "`C"
+        temp = Number(textBox.value);
+        temp = ((temp - 32) * 5) / 9;
+        result.textContent = temp.toFixed(1) + "'C";
     } else {
-        result.textContent = "Please a Unit"
+        result.textContent = "Please select a unit";
     }
-}) 
+});
