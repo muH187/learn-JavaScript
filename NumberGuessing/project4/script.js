873b1bd2882d52174d01e4f0a9f826826727ec4e
Muhammad Ali <proqualitythings100@gmail.com>
2024-01-21 16:23:13 +0500

    correction

--- NumberGuessing/project4/script.js
+++ NumberGuessing/project4/script.js
@@ -1,13 +1,14 @@
 const minNum = 1
 const maxNum = 100
 
-
+let randNum = Math.floor(Math.random() * 100) + 1
+let userInput
 let running = true
 
 while(running) {
-    const userInput = window.prompt(`Enter number between ${minNum}-${maxNum}`)
+    userInput = window.prompt(`Enter number between ${minNum}-${maxNum}`)
     userInput = Number(userInput)
-    const randNum = Math.floor(Math.random() * 100) + 1
+    
 
     if (userInput < randNum) {
         window.alert("TOO LOW! Guess Again.")
