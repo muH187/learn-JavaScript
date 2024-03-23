c921b59faa35fdba7f03ffeefe50f857ff0764f9
Muhammad Ali <proqualitythings100@gmail.com>
2024-03-23 13:16:46 +0500

    again start learning js

---
+++ CallPromise2/script.js
@@ -0,0 +1,8 @@
+console.log("Ali is an Entreprenuer");
+console.log("Ali is a multi millionaire");
+
+setTimeout(() => {
+    console.log("Ali is 19 years old.");
+}, 2000)
+
+console.log("The End...");
---
+++ InsertHtml/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".box1").innerHTML = "I'm doing javascript practice"
+document.querySelector(".box3").innerHTML = "Wo jiao Cong Ming"
+
+// let div = document.createElement("div")
+// div.innerHTML = "<h1>I have been inserted by <b>Ali</b></h1>"
+// div.setAttribute("class", "created")
+// document.querySelector(".container").before(div)
+
+// let cont = document.querySelector(".container")
+// cont.insertAdjacentHTML("beforeend", "<b>I'm under the water. Please help me here too much raining... ioooooouch</b>")
+
+document.querySelector(".container").classList
