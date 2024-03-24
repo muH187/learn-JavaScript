4ae606b0164451f4b61cafe3f802c8ba942e7112
Muhammad Ali <proqualitythings100@gmail.com>
2024-03-24 13:20:15 +0500

    promises

---
+++ AsyncAwait/script.js
@@ -0,0 +1,61 @@
+// const ticket = new Promise(function (resolve, reject) {
+//     const isBoarded = false
+//     if(isBoarded) {
+//         resolve("You are in the flight...")
+//     } else {
+//         reject("You flight has been cancelled")
+//     }
+// })
+
+// ticket.then((data) => {
+//     console.log("Woohoo!", data);
+// }).catch((data) => {
+//     console.log("Oh Noo.", data);
+// }).finally(() => {
+//     console.log("I will always be executed");
+// })
+
+function getCheese() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const cheese = "🧀"
+            resolve(cheese)
+        }, 2000);
+    })
+}
+
+function getDough(cheese) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        const dough = cheese + "🍽️"
+        // resolve(dough)
+        reject("Electricity Gone")
+        }, 2000);
+        
+    })
+}
+
+function bakePizza(dough) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const pizza = dough + "🍕"
+            resolve(pizza)
+        }, 2000);
+    })
+}
+
+getCheese()
+    .then((cheese) => {
+        console.log("Here is your cheese", cheese);
+        return getDough(cheese);
+    })
+    .then((dough) => {
+        console.log("Here is your dough", dough);
+        return bakePizza(dough)
+    })
+    .then((pizza) => {
+        console.log("Here is your pizza", pizza);
+    })
+    .catch((data) => {
+    console.log("Error Occured", data);
+})
