bb8ec3728a5769a130f31dbc125ac3363b0950e1
Muhammad Ali <proqualitythings100@gmail.com>
2024-08-13 23:30:04 +0500

    destructuring

--- Destructuring/.js
+++ Destructuring/.js
@@ -3,6 +3,9 @@ let arr = [2, 4, 12, 100, 23, 43, 53]
 
 // console.log(a, b, c, d, ...rest)
 
-let [a, , , , ...rest] = arr
+// let [a, , , , ...rest] = arr
 
-console.log(a, ...rest)
+// console.log(a, ...rest)
+
+let {a, b} = {a: 1, b: 2}
+console.log(a, b)
