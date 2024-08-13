482f372d2f1a8c6a344d9b048bb4ed5fc694ade9
Muhammad Ali <proqualitythings100@gmail.com>
2024-08-13 15:46:44 +0500

    spread operator

---
+++ SpreadOperator/.js
@@ -0,0 +1,10 @@
+let fruits = ["Mango", "Banana", "Grapes", "Kivi"]
+let vegetables = ["Potatoes", "Onion", "Tomatoe", "Cereal"]
+
+let foods = [...fruits, ...vegetables, "Egg", "Milk"]
+
+console.log(foods)
+
+let name = "Muhammad Ali"
+let myName = [...name]
+console.log(myName)
