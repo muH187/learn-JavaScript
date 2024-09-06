a46c696edbc75e2d33f6392b33df4c12b82c3c6f
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-06 12:23:01 +0500

    destructuring

---
+++ Destructuring/array.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3]
+const [num1, num2, num3] = numbers
+console.log(num2)
+
+const students = ["Ali", "Mirza", "Umer", "Basit", "Yasir"]
+const [student1, , , , student5] = students
+console.log(student1, student5)
+
+// let fruit1 = "Mango"
+// let fruit2 = "Banana"
+
+// [fruit1, fruit2] = [fruit2, fruit1]
+
+// console.log(fruit1)
+// console.log(fruit2)
+
