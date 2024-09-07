fc7c9f009017d9ad0adf06fbd818cdc534f76456
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-07 11:43:52 +0500

    &&

---
+++ logicalOperators/&&.js
@@ -0,0 +1,17 @@
+let age = 29
+let hasDrivingLicence = true
+
+if(age >= 18 && hasDrivingLicence) {
+    console.log("You can drive.")
+} else {
+    console.log("You are not elligible to drive.")
+}
+
+let isWeekend = true
+let isSunny = false
+
+if(isWeekend && isSunny) {
+    console.log("Perfect Time to go to picnic.")
+} else {
+    console.log("Stay at home.")
+}
