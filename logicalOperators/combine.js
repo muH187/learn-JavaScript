b2f759510abd8ef5480b5bb802b88c8a7b6f0752
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-07 11:56:18 +0500

    combine

---
+++ logicalOperators/combine.js
@@ -0,0 +1,19 @@
+let isHoliday = false
+let isWeekend = true
+let isSunny = true
+
+if((isHoliday || isWeekend) && isSunny) {
+    console.log("Let's go to picnic.")
+} else {
+    console.log("Stay home. Work Hard.")
+}
+
+let hasTicket = false
+let hasId = false
+let isVip = true
+
+if((hasTicket && hasId) || isVip) {
+    console.log("You can enter.")
+} else {
+    console.log("You can not enter.")
+}
