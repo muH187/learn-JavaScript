4bdb60accfda5335686c3e97a5be66a9444f6a9e
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-07 11:47:23 +0500

    ||

---
+++ logicalOperators/||.js
@@ -0,0 +1,16 @@
+let hasSavings = true
+let hasInvestments = false
+
+if(hasSavings || hasInvestments) {
+    console.log("You are financially secure.")
+} else {
+    console.log("You need to save or invest.")
+}
+
+let temperature = 10
+
+if(temperature > 30 || temperature < 0) {
+    console.log("Extreme Weather.")
+} else {
+    console.log("Weather is good.")
+}
