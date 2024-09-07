f59161868da0369ccc0a89b5b1c26f27bcc9aa17
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-07 11:52:29 +0500

    !

---
+++ logicalOperators/!=.js
@@ -0,0 +1,15 @@
+let isRaining = false
+
+if(!isRaining){
+    console.log("Go Outside")
+} else {
+    console.log("Stay indoors.")
+}
+
+let isLoggedIn = true
+
+if(isLoggedIn) {
+    console.log("Please log in.")
+} else {
+    console.log("You are logged in.")
+}
