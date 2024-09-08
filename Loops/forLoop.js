390edc23c74c60440c111ece2abe5ad37f2eeb4b
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-08 10:15:41 +0500

    for loop

---
+++ Loops/forLoop.js
@@ -0,0 +1,42 @@
+let sum = 0; // Initialize the sum to 0
+
+for (let i = 1; i <= 100; i++) {
+  sum += i; // Add the current number to the sum
+}
+
+console.log("The sum of numbers from 1 to 100 is:", sum);
+
+
+let count = 0
+
+for (let i = 1; i < 5; i++) {
+    count += i
+}
+console.log(count)
+
+{
+    let table = 7
+    for (let i = 1; i < 11; i++) {
+        console.log(`7 x ${i} = ${i * 7}`)
+    }
+}
+{
+    for (let i = 2; i < 21; i += 2) {
+        console.log(i)
+    }
+}
+{
+    let factorial = 1
+    for (let i = 1; i < 8; i++) {
+        factorial *= i
+    }
+    console.log(`The factorial of 7 is: ${factorial}`)
+}
+{
+    let string = "Hello"
+    let reversed = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i]
+    }
+    console.log(reversed)
+}
