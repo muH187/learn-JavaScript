628a1145556e1979e8b1695c0050737562fd0ed0
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-08 08:53:53 +0500

    practice slice

--- SliceSplice/slice.js
+++ SliceSplice/slice.js
@@ -8,3 +8,18 @@
     const newArray = array.slice(0, 3)
     console.log(newArray)
 }
+{
+    const array = ['dog', 'cat', 'rabbit', 'hamster']
+    const newArray = array.slice(2, 4)
+    console.log(newArray)
+}
+{
+    const array = [100, 200, 300]
+    const newArray = array.slice(0)
+    console.log(newArray)
+}
+{
+    const array = [1, 2, 3, 4, 5, 6]
+    const newArray = array.slice(0, )
+    console.log(newArray)
+}
