a808168e115f8b49b5cc75cfa1b53be32bb25e5d
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-08 09:00:56 +0500

    practice splice

---
+++ SliceSplice/splice.js
@@ -0,0 +1,25 @@
+{
+    const array = [1, 2, 3, 6, 7]
+    array.splice(3, 2, 4, 5)
+    console.log(array)
+}
+{
+    const array = ['apple', 'pear', 'banana', 'grape']
+    array.splice(1, 2)
+    console.log(array)
+}
+{
+    const array = ['apple', 'orange', 'banana']
+    array.splice(2, 0, "kivi", "melon")
+    console.log(array)
+}
+{
+    const array = [10, 20, 30, 40, 50]
+    array.splice(0, 2)
+    console.log(array)
+}
+{
+    const colors = ['red', 'green', 'blue']
+    colors.splice(1, 1, 'yellow')
+    console.log(colors)
+}
