b2f79196c19ee0e2fd43332651fd5de815aa75eb
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-08 08:49:29 +0500

    practicing slicing

---
+++ SliceSplice/slice.js
@@ -0,0 +1,10 @@
+{
+    const array = ['a', 'b', 'c', 'd', 'e']
+    const newArray = array.slice(1, 4)
+    console.log(newArray)
+}
+{
+    const array = [10, 20, 30, 40, 50]
+    const newArray = array.slice(0, 3)
+    console.log(newArray)
+}
