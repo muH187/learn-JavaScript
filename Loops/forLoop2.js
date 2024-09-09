31dd2b8fdbc6c74a576c0d8cd0fe95552bf01549
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-09 11:01:10 +0500

    diamond pattern

--- Loops/forLoop2.js
+++ Loops/forLoop2.js
@@ -41,3 +41,30 @@ console.log("reverse pyramid")
         console.log(pattern)
     }
 }
+
+console.log("Diamond Pattern")
+
+{
+    let n = 5
+    for (let i = 1; i <= n; i++) {
+        let pattern = ""
+        for (let j = 0; j < n - i; j++) {
+            pattern += " "
+        }
+        for (let k = 0; k <  2 * i - 1; k++) {
+            pattern += "*"
+        }
+        console.log(pattern)
+    }
+    for (let i = n - 1; i >= 1; i--) {
+        let pattern = ""
+        for (let j = 0; j < n - i; j++) {
+            pattern += " "
+        }
+        for (let k = 0; k < 2 * i - 1; k++) {
+            pattern += "*"
+        }
+        console.log(pattern)
+    }
+
+}
